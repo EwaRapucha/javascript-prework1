feda17e839b10b3f08c8f7e584a59570d16e60b4
Ewa Rapucha <annarapucha@gmail.com>
2022-10-20 11:42:08 +0100

    Add .gitignore

--- js/script.js
+++ js/script.js
@@ -2,15 +2,15 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
+let computerMove = getMoveName(randomNumber);
 
-if(randomNumber == 1){
+/*if(randomNumber == 1){
   computerMove = 'kamień';
 } else if (randomNumber == 2){
     computerMove = 'papier';
 } else if (randomNumber == 3){
     computerMove = 'nożyce'; 
-}
+}*/
 
 printMessage('Mój ruch to: ' + computerMove);
 
