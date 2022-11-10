89cd57dc219384b58111d61edf7d5ddeea30b4ce
Ewa Rapucha <annarapucha@gmail.com>
2022-11-10 12:55:34 +0000

    Change js/script.js

--- js/script.js
+++ js/script.js
@@ -72,17 +72,17 @@
     displayResult(computerMove, playerMove);
   }
 
-  let playRock = document.getElementById("play-rock");
+  const playRock = document.getElementById("play-rock");
   playRock.addEventListener("click", function () {
     playGame(1);
   });
 
-  let playPaper = document.getElementById("play-paper");
+  const playPaper = document.getElementById("play-paper");
   playPaper.addEventListener("click", function () {
     playGame(2);
   });
 
-  let playScissors = document.getElementById("play-scissors");
+  const playScissors = document.getElementById("play-scissors");
   playScissors.addEventListener("click", function () {
     playGame(3);
   });
